40e8defd2f23cbf57ae0b65c120a723b39c7f3df
Yash Rohilla <yr322005@gmail.com>
2025-04-23 23:39:36 +0530

    link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
     setError("");
 
     try {
-      const response = await fetch("http://https://crop-backend-cscv.onrender.com/:5000/predict", {
+      const response = await fetch("http://https://crop-backend-cscv.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
